5ee7e094ec8e7e9a99a1320bec7ff465eb07ba26
Elmeri Lehikoinen <elmeri.lehikoinen@gmail.com>
2017-03-20 15:07:36 +0200

    ui update

--- MatrixCalc/src/matrixcalc/logic/Matrix.java
+++ MatrixCalc/src/matrixcalc/logic/Matrix.java
@@ -4,4 +4,14 @@ package matrixcalc.logic;
 
 public class Matrix {
     
+    public Matrix() {
+        
+        //Create cells for values
+        int c11, c12, c13,
+            c21, c22, c23,
+            c31, c32, c33;
+        
+        
+        
+    }
 }
--- MatrixCalc/src/matrixcalc/ui/UserInterface.java
+++ MatrixCalc/src/matrixcalc/ui/UserInterface.java
@@ -3,6 +3,7 @@ package matrixcalc.ui;
 
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 
@@ -10,6 +11,18 @@ public class UserInterface implements Runnable{
 
     JFrame frame;
     
+    //Textfields for matrix cells (3x3)
+    JTextField a11, a12, a13,
+               a21, a22, a23,
+               a31, a32, a33,
+            
+               b11, b12, b13,
+               b21, b22, b23,
+               b31, b32, b33;
+    
+    //Temporary array for matrix cell creation
+    JTextField[] cellContainer = {a11, a12, a13, a21, a22, a23, a31, a32, a33, 
+                                  b11, b12, b13, b21, b22, b23, b31, b32, b33};
     
     @Override
     public void run() {
@@ -23,11 +36,55 @@ public class UserInterface implements Runnable{
         
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
+        //Create visible Matrix cells (JTextFields)
+        createCells(frame);
         
         frame.setVisible(true);
         
         frame.pack();
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    void createCells(JFrame frame) {
+        
+        //Cells x and y coordinations
+        int x = 40;
+        int y = 40;
+        
+        //Used to assign cells to right rows
+        int rowCounter = 0;
+        
+        //Creates each cell to specific row and adds them to JFrame
+        for (JTextField cell : cellContainer) {
+            
+            //Sets correct coordinates for each cell
+            if (rowCounter == 3 || rowCounter == 6) {
+                y += 35;
+                x = 40;
+            } else if (rowCounter == 9){
+                y = 40;
+                x = 225;
+            } else if (rowCounter == 12 || rowCounter == 15) {
+                y += 35;
+                x = 225;
+            }
+            
+            //Create new cell
+            cell = new JTextField();
+            
+            //Set cell coordinates
+            cell.setBounds(x, y, 35, 25);
+            
+            //Add cell to JFrame
+            frame.add(cell);
+            
+            //Change new coordinates and row number
+            x += 50;
+            rowCounter++;
+        }
+        
+        
+        
     }
     
 }
